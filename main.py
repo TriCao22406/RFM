48169b16dbaedf90781075a299cb3eda3f4b3968
HungTran284 <148221758+HungTran284@users.noreply.github.com>
2023-10-18 15:22:06 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def rfm(inputfile, outputfile, ngay_can_tinh):
     print(" tính toán RFM vào ngày " + ngay_can_tinh)
     print("---------------------------------------------")
 
-    # Chuyển đổi ngay_can_tinh thành đối tượng datetime với định dạng %Y-%m-%d
     NOW = datetime.strptime(ngay_can_tinh, "%Y-%m-%d")
 
     #tạo dataframe don_hang
@@ -18,35 +17,29 @@ def rfm(inputfile, outputfile, ngay_can_tinh):
     #chuyển dữ liệu chuỗi ngày đặt mua thành dữ liệu kiểu ngày tháng
     don_hang['ngay_dat_mua'] = pd.to_datetime(don_hang['ngay_dat_mua'])
     #nhóm các dữ liệu về khách hàng lại và tính toán
-    # Nhóm khung dữ liệu don_hang theo cột 'khach_hang'
-    # agg() áp dụng các hàm sau cho mỗi nhóm dữ liệu:
-        # lambda x: (NOW - x.max()).days tính số ngày kể từ lần mua hàng cuối cùng của khách hàng.
-        # lambda x: len(x) tính số lần mua hàng của khách hàng.
-        # lambda x: x.sum() tính tổng giá trị đơn hàng của khách hàng.
     bang_rfm = don_hang.groupby('khach_hang').agg({'ngay_dat_mua': lambda x: (NOW - x.max()).days,  # Recency
                                                'so_lan_mua_hang': lambda x: len(x),  # Frequency
                                                'tong_gia_tri_hang': lambda x: x.sum()})  # Monetary Value
-    # Chuyển đổi dữ liệu trong cột ngay_dat_mua của bảng RFM thành kiểu dữ liệu int
+
     #bang_rfm['ngay_dat_mua'] = bang_rfm['ngay_dat_mua'].astype(int)
-    # đổi tên các cột trong bảng RFM lần lượt thành recency, frequency và monetary_value
+    #đổi tên để phù hợp với bảng rfm
     bang_rfm.rename(columns={'ngay_dat_mua': 'recency',
                              'so_lan_mua_hang': 'frequency',
                              'tong_gia_tri_hang': 'monetary_value'}, inplace=True)
-    #tính toán tứ phân vị cho mỗi cột trong bảng RFM
-    tu_phan_vi = bang_rfm.quantile(q=[0.25, 0.5, 0.75])
-    # Chuyển đổi kết quả tính toán thành một dictionary
-    tu_phan_vi = tu_phan_vi.to_dict()
+    #tính toán ngũ phân vị
+    ngu_phan_vi = bang_rfm.quantile(q=[0.2, 0.4, 0.6, 0.8])
+    ngu_phan_vi = ngu_phan_vi.to_dict()
 
     phan_doan_khach_hang = bang_rfm
-    #gán cột tu_phan_vi cho df phan_doan_khach_hang và tính tứ phân vị các mục bằng cách thực hiện hàm
-    phan_doan_khach_hang['tu_phan_vi_R'] = phan_doan_khach_hang['recency'].apply(tu_phan_vi_recency, args=('recency', tu_phan_vi,))
-    phan_doan_khach_hang['tu_phan_vi_F'] = phan_doan_khach_hang['frequency'].apply(tu_phan_vi_frequency, args=('frequency', tu_phan_vi,))
-    phan_doan_khach_hang['tu_phan_vi_M'] = phan_doan_khach_hang['monetary_value'].apply(tu_phan_vi_monetary_value, args=('monetary_value', tu_phan_vi,))
+    #gán cột ngu_phan_vi cho df phan_doan_khach_hang và tính ngũ phân vị các mục bằng cách thực hiện hàm
+    phan_doan_khach_hang['ngu_phan_vi_R'] = phan_doan_khach_hang['recency'].apply(ngu_phan_vi_recency, args=('recency', ngu_phan_vi,))
+    phan_doan_khach_hang['ngu_phan_vi_F'] = phan_doan_khach_hang['frequency'].apply(ngu_phan_vi_frequency, args=('frequency', ngu_phan_vi,))
+    phan_doan_khach_hang['ngu_phan_vi_M'] = phan_doan_khach_hang['monetary_value'].apply(ngu_phan_vi_monetary_value, args=('monetary_value', ngu_phan_vi,))
 
     #tổng kết về RFM khách hàng đó
-    phan_doan_khach_hang['tong_ket_RFM'] = phan_doan_khach_hang.tu_phan_vi_R.map(str) + \
-                                           phan_doan_khach_hang.tu_phan_vi_F.map(str) + \
-                                           phan_doan_khach_hang.tu_phan_vi_M.map(str)
+    phan_doan_khach_hang['tong_ket_RFM'] = phan_doan_khach_hang.ngu_phan_vi_R.map(str) + \
+                                           phan_doan_khach_hang.ngu_phan_vi_F.map(str) + \
+                                           phan_doan_khach_hang.ngu_phan_vi_M.map(str)
 
     phan_doan_khach_hang.to_csv(outputfile, sep=',')
 
@@ -54,38 +47,43 @@ def rfm(inputfile, outputfile, ngay_can_tinh):
     print(" Hoàn tất! Kiểm tra %s" % (outputfile))
     print(" ")
 
-# x = giá trị phân loại tứ phân vị, p = giá trị recency, d = dict tứ phân vị
-def tu_phan_vi_recency(x, p, d):
-    if x <= d[p][0.25]:
+# x = giá trị phân loại ngũ phân vị, p = giá trị recency, d = dict ngũ phân vị
+def ngu_phan_vi_recency(x, p, d):
+    if x <= d[p][0.2]:
+        return 5
+    elif x <= d[p][0.4]:
         return 4
-    elif x <= d[p][0.50]:
+    elif x <= d[p][0.6]:
         return 3
-    elif x <= d[p][0.75]:
+    elif x <= d[p][0.8]:
         return 2
     else:
         return 1
 
 
-# x = giá trị phân loại tứ phân vị, p = giá trị frequency, d = dict tứ phân vị
-def tu_phan_vi_frequency(x, p, d):
-    if x <= d[p][0.25]:
+# x = giá trị phân loại ngũ phân vị, p = giá trị frequency, d = dict ngũ phân vị
+def ngu_phan_vi_frequency(x, p, d):
+    if x <= d[p][0.2]:
         return 1
-    elif x <= d[p][0.50]:
+    elif x <= d[p][0.4]:
         return 2
-    elif x <= d[p][0.75]:
+    elif x <= d[p][0.6]:
         return 3
-    else:
+    elif x <= d[p][0.8]:
         return 4
+    else:
+        return 5
 
-# x = giá trị phân loại tứ phân vị, p = giá trị monetary_value, d = dict tứ phân vị
-def tu_phan_vi_monetary_value(x, p, d):
-    if x <= d[p][0.25]:
+# x = giá trị phân loại ngũ phân vị, p = giá trị monetary_value, d = dict ngũ phân vị
+def ngu_phan_vi_monetary_value(x, p, d):
+    if x <= d[p][0.2]:
         return 1
-    elif x <= d[p][0.50]:
+    elif x <= d[p][0.4]:
         return 2
-    elif x <= d[p][0.75]:
+    elif x <= d[p][0.6]:
         return 3
-    else:
+    elif x <= d[p][0.8]:
         return 4
-
-rfm("sample-orders.csv","output.csv","2023-10-17")
+    else:
+        return 5
+rfm("sample-orders.csv","output.csv","2022-12-31")
