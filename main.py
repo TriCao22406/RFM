b0384c54f86eaba338b9544d80a69c14a2a0d745
HungTran284 <148221758+HungTran284@users.noreply.github.com>
2023-10-18 10:04:44 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,73 +4,81 @@ import pandas as pd
 from datetime import datetime
 
 
-def rfm(inputfile, outputfile, inputdate):
+def rfm(inputfile, outputfile, ngay_can_tinh):
     print(" ")
     print("---------------------------------------------")
-    print(" Calculating RFM segmentation for " + inputdate)
+    print(" tính toán RFM vào ngày " + ngay_can_tinh)
     print("---------------------------------------------")
 
-    NOW = datetime.strptime(inputdate, "%Y-%m-%d")
+    NOW = datetime.strptime(ngay_can_tinh, "%Y-%m-%d")
 
-    # Open orders file
-    orders = pd.read_csv(inputfile, sep=',', encoding="windows-1252")
-    orders['order_date'] = pd.to_datetime(orders['order_date'])
+    #tạo dataframe don_hang
+    don_hang = pd.read_csv(inputfile, sep=',', encoding="windows-1252")
+    #chuyển dữ liệu chuỗi ngày đặt mua thành dữ liệu kiểu ngày tháng
+    don_hang['ngay_dat_mua'] = pd.to_datetime(don_hang['ngay_dat_mua'])
+    #nhóm các dữ liệu về khách hàng lại và tính toán
+    bang_rfm = don_hang.groupby('khach_hang').agg({'ngay_dat_mua': lambda x: (NOW - x.max()).days,  # Recency
+                                               'so_lan_mua_hang': lambda x: len(x),  # Frequency
+                                               'tong_gia_tri_hang': lambda x: x.sum()})  # Monetary Value
 
-    rfmTable = orders.groupby('customer').agg({'order_date': lambda x: (NOW - x.max()).days,  # Recency
-                                               'order_id': lambda x: len(x),  # Frequency
-                                               'grand_total': lambda x: x.sum()})  # Monetary Value
+    #bang_rfm['ngay_dat_mua'] = bang_rfm['ngay_dat_mua'].astype(int)
+    #đổi tên để phù hợp với bảng rfm
+    bang_rfm.rename(columns={'ngay_dat_mua': 'recency',
+                             'so_lan_mua_hang': 'frequency',
+                             'tong_gia_tri_hang': 'monetary_value'}, inplace=True)
+    #tính toán tứ phân vị
+    tu_phan_vi = bang_rfm.quantile(q=[0.25, 0.5, 0.75])
+    tu_phan_vi = tu_phan_vi.to_dict()
 
-    rfmTable['order_date'] = rfmTable['order_date'].astype(int)
-    rfmTable.rename(columns={'order_date': 'recency',
-                             'order_id': 'frequency',
-                             'grand_total': 'monetary_value'}, inplace=True)
+    phan_doan_khach_hang = bang_rfm
+    #gán cột tu_phan_vi cho df phan_doan_khach_hang và tính tứ phân vị các mục bằng cách thực hiện hàm
+    phan_doan_khach_hang['tu_phan_vi_R'] = phan_doan_khach_hang['recency'].apply(tu_phan_vi_recency, args=('recency', tu_phan_vi,))
+    phan_doan_khach_hang['tu_phan_vi_F'] = phan_doan_khach_hang['frequency'].apply(tu_phan_vi_frequency, args=('frequency', tu_phan_vi,))
+    phan_doan_khach_hang['tu_phan_vi_M'] = phan_doan_khach_hang['monetary_value'].apply(tu_phan_vi_monetary_value, args=('monetary_value', tu_phan_vi,))
 
-    quintiles = rfmTable.quantile(q=[0.20, 0.40, 0.60, 0.80])
-    quintiles = quintiles.to_dict()
+    #tổng kết về RFM khách hàng đó
+    phan_doan_khach_hang['tong_ket_RFM'] = phan_doan_khach_hang.tu_phan_vi_R.map(str) + \
+                                           phan_doan_khach_hang.tu_phan_vi_F.map(str) + \
+                                           phan_doan_khach_hang.tu_phan_vi_M.map(str)
 
-    rfmSegmentation = rfmTable
-
-    rfmSegmentation['R_Quintile'] = rfmSegmentation['recency'].apply(RClass, args=('recency', quintiles,))
-    rfmSegmentation['F_Quintile'] = rfmSegmentation['frequency'].apply(FMClass, args=('frequency', quintiles,))
-    rfmSegmentation['M_Quintile'] = rfmSegmentation['monetary_value'].apply(FMClass,
-                                                                            args=('monetary_value', quintiles,))
-
-    rfmSegmentation['RFMScore'] = rfmSegmentation.R_Quartile.map(str) + rfmSegmentation.F_Quartile.map(
-        str) + rfmSegmentation.M_Quartile.map(str)
-
-    rfmSegmentation.to_csv(outputfile, sep=',')
+    phan_doan_khach_hang.to_csv(outputfile, sep=',')
 
     print(" ")
-    print(" DONE! Check %s" % (outputfile))
+    print(" Hoàn tất! Kiểm tra %s" % (outputfile))
     print(" ")
 
-
-# We create two classes for the RFM segmentation since, being high recency is bad, while high frequency and monetary value is good.
-# Arguments (x = value, p = recency, k = quartiles dict)
-def RClass(x, p, d):
-    if x <= d[p][0.20]:
-        return 5
-    elif x <= d[p][0.40]:
+# x = giá trị phân loại tứ phân vị, p = giá trị recency, d = dict tứ phân vị
+def tu_phan_vi_recency(x, p, d):
+    if x <= d[p][0.25]:
         return 4
-    elif x <= d[p][0.60]:
+    elif x <= d[p][0.50]:
         return 3
-    elif x <= d[p][0.80]:
+    elif x <= d[p][0.75]:
         return 2
     else:
         return 1
 
 
-# Arguments (x = value, p = monetary_value, frequency, k = quartiles dict)
-def FMClass(x, p, d):
-    if x <= d[p][0.20]:
+# x = giá trị phân loại tứ phân vị, p = giá trị frequency, d = dict tứ phân vị
+def tu_phan_vi_frequency(x, p, d):
+    if x <= d[p][0.25]:
         return 1
-    elif x <= d[p][0.40]:
+    elif x <= d[p][0.50]:
         return 2
-    elif x <= d[p][0.60]:
+    elif x <= d[p][0.75]:
         return 3
-    elif x <= d[p][0.80]:
-        return 4
     else:
-        return 5
+        return 4
 
-rfm("sample-orders.csv","output.csv","2022-12-31")
+# x = giá trị phân loại tứ phân vị, p = giá trị monetary_value, d = dict tứ phân vị
+def tu_phan_vi_monetary_value(x, p, d):
+    if x <= d[p][0.25]:
+        return 1
+    elif x <= d[p][0.50]:
+        return 2
+    elif x <= d[p][0.75]:
+        return 3
+    else:
+        return 4
+
+rfm("sample-orders.csv","output.csv","2023-10-17")
